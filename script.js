fcc23f69a3a7d4d04a7a750b2f0c696b6c128a16
gchristiansun <167200722+gchristiansun@users.noreply.github.com>
2024-11-07 23:33:41 +0700

    rev

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 
 const container = document.querySelector('main');
-const section = document.querySelector('.about-section h2')
+const section = document.querySelector('.container-particle-section')
 
 const numParticles = 300;
+const numParticles2 = 600;
 
 for (let i = 0; i < numParticles; i++) {
   const particle = document.createElement('div');
@@ -22,7 +23,29 @@ for (let i = 0; i < numParticles; i++) {
 
   // Tambahkan partikel ke dalam container
   container.appendChild(particle);
-  // section.appendChild(particle)
+}
+
+for (let i = 0; i < numParticles2; i++) {
+  const particle2 = document.createElement('div');
+  particle2.classList.add('particle');
+
+  const containerHeight = container.offsetHeight;
+  const containerWidth = container.offsetWidth;
+
+  // Set posisi acak untuk setiap partikel
+  const p = Math.random() * 100; // 0-100% dari lebar konten
+  const q = Math.random() * 100; // 0-100% dari tinggi konten
+  particle2.style.left = `${p}%`;
+  particle2.style.top = `${q}%`;
+
+  // Ubah durasi dan delay animasi secara acak agar tidak semuanya bergerak serempak
+  const animationDuration = (Math.random() * 5 + 3).toFixed(2); // antara 3s - 8s
+  const animationDelay = (Math.random() * 5).toFixed(2); // antara 0s - 5s
+  particle2.style.animationDuration = `${animationDuration}s`;
+  particle2.style.animationDelay = `${animationDelay}s`;
+
+  // Tambahkan partikel ke dalam container
+  section.appendChild(particle2);
 }
 
 const buttonmenu = document.querySelector('.icon-nav');
