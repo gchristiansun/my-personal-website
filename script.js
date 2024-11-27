b43c0ae87771cbba4c88d077f8c8d4839083627f
gchristiansun <167200722+gchristiansun@users.noreply.github.com>
2024-11-27 20:15:45 +0700

    Remove jurnal side bar

--- script.js
+++ script.js
@@ -103,17 +103,3 @@ buttonSwitch.addEventListener('click', () => {
 //   element.setAttribute('data-aos', 'fade-up'); // Tambahkan animasi fade-up
 //   element.setAttribute('data-aos-delay', index * 5); // Tambahkan delay berdasarkan urutan
 // });
-
-var cardJurnal = document.querySelector('.card-jurnal');
-window.addEventListener('scroll', function() {
-  if (window.innerHeight + window.scrollY >= document.documentElement.scrollHeight - 400) {
-    cardJurnal.style.transform = 'translateX(-100%)'
-  }
-  else if (window.scrollY > 700) {
-    cardJurnal.style.transform = 'translateX(0)'
-  } 
-  else {
-      cardJurnal.style.transform = 'translateX(-100%)'
-  }
-  });
-
